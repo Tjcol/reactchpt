34de137cabe69af89cf4bb9bbff349d304122897
Tjcol <41172230+Tjcol@users.noreply.github.com>
2024-09-21 15:11:39 +0100

    gxf

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      Person: {
+        fullName: 'Oghenetejiri Onoriose',
+        bio: 'A passionate web developer with a focus on Shopify and full-stack development.',
+        imgSrc: 'https://via.placeholder.com/150',
+        profession: 'Web Developer',
+      },
+      shows: false,
+      mountedTime: 0,
+    };
+    this.toggleShow = this.toggleShow.bind(this);
+  }
+
+  toggleShow() {
+    this.setState({ shows: !this.state.shows });
+  }
+
+  componentDidMount() {
+    this.timerID = setInterval(() => {
+      this.setState((prevState) => ({
+        mountedTime: prevState.mountedTime + 1,
+      }));
+    }, 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+
+  render() {
+    const { Person, shows, mountedTime } = this.state;
+    return (
+      <div className="App">
+        <button onClick={this.toggleShow}>
+          {shows ? 'Hide Profile' : 'Show Profile'}
+        </button>
+
+        {shows && (
+          <div className="profile">
+            <img src={Person.imgSrc} alt={Person.fullName} style={{ width: '150px', borderRadius: '50%' }} />
+            <h2>{Person.fullName}</h2>
+            <p>{Person.bio}</p>
+            <h3>{Person.profession}</h3>
+          </div>
+        )}
+
+        <div className="time-interval">
+          <p>Component mounted for: {mountedTime} seconds</p>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
